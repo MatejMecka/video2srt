a05a0f83371bab82ee45ec5b5c780208f40a8ca6
Matej Plavevski <matej.plavevski@gmail.com>
2024-02-14 18:20:44 +0200

    Update entrypoint.sh

--- .github/actions/pwa2Apk/entrypoint.sh
+++ .github/actions/pwa2Apk/entrypoint.sh
@@ -5,5 +5,7 @@ cd $1
 echo "========================= Building APK, it may take 3-4 minutes or more ========================="
 ( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | bubblewrap build --skipPwaValidation --skipSigning
 cp ./*.apk ..
+ls
+tree
 cp ./app/build/outputs/bundle/release/*.aab ..
 echo "========================= APK building finished ========================="
